2f605ae6712066dc53e62adcef8c100f319128c1
nord <khalidimran7860q@gmail.com>
2026-01-28 20:53:24 +0530

    removed build

--- build/CMakeFiles/Code_hour.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Code_hour.
-# This may be replaced when dependencies are built.
--- build/Desktop_Qt_6_9_2-Debug/.qt/deploy_Code_hour_dcc014f48e.cmake
+++
@@ -1,8 +0,0 @@
-include("/home/nord/Project_Data/Code_Hours/Code_hour/build/Desktop_Qt_6_9_2-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Code_hour-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase;qtdeclarative;qtdeclarative;qtdeclarative;qtdeclarative;qtdeclarative;qtdeclarative;qtdeclarative;qtwebengine;qtdeclarative;qtwebengine")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "/home/nord/Project_Data/Code_Hours/Code_hour/build/Desktop_Qt_6_9_2-Debug/Code_hour"
-    GENERATE_QT_CONF
-)
